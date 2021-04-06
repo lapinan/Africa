7161890c275070b6b6ebea27a93d8d5d6c5cd6a2
Andrey <lapinandreyly@bk.ru>
2021-04-06 23:25:52 +0300

    add GridView

--- Africa/View/Gallery/GalleryView.swift
+++ Africa/View/Gallery/GalleryView.swift
@@ -6,15 +6,66 @@
 //
 
 import SwiftUI
+import AudioToolbox
 
 struct GalleryView: View {
+  @State fileprivate var selectedAnimal: String = "lion"
+  @State fileprivate var gridColumn: Double = 3.0
+  @State fileprivate var items: [GridItem] = [GridItem(.flexible())]
+  
+  fileprivate let animals: [Animal] = Bundle.main.decode("animals.json")
+  
     var body: some View {
       ScrollView(showsIndicators: false) {
-        
+        VStack(alignment: .center, spacing: 30) {
+          Image(selectedAnimal)
+            .resizable()
+            .scaledToFit()
+            .clipShape(Circle())
+            .overlay(
+              Circle()
+                .stroke(Color.white, lineWidth: 8)
+            )
+          
+          Slider(value: $gridColumn, in: 2...4, step: 1)
+            .padding(.horizontal)
+            .onChange(of: gridColumn, perform: { value in
+              gridSwitch()
+              AudioServicesPlaySystemSound(1519)
+            })
+          
+          LazyVGrid(columns: items, alignment: .center, spacing: 10) {
+            ForEach(animals) { animal in
+              Image(animal.image)
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(
+                  Circle()
+                    .stroke(Color.white, lineWidth: 1)
+                )
+                .onTapGesture {
+                  selectedAnimal = animal.image
+                }
+            }
+          }
+          .animation(.easeIn)
+          .onAppear(perform: {
+            gridSwitch()
+          })
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 50)
       }
       .background(MotionAnimationView())
-      .edgesIgnoringSafeArea(.top)
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+}
+
+extension GalleryView {
+  fileprivate func gridSwitch() {
+    items = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+  }
 }
 
 struct GalleryView_Previews: PreviewProvider {
